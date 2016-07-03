c2034a7135d09bbe8d47335d37db1a17350781ea
Ostanik <alan.ostanik@socialbase.com.br>
2016-07-03 19:45:32 -0300

    Teste de UI (snapshot-fastlane)

--- LoLChampionsUITests/LoLChampionsUITests.swift
+++ LoLChampionsUITests/LoLChampionsUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class LoLChampionsUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
@@ -19,7 +19,7 @@ class LoLChampionsUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -31,6 +31,54 @@ class LoLChampionsUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let arrItens = ["Aatrox", "Ahri", "Akali", "Alistar"]
+        
+        //Go for the four first itens.
+        for item in arrItens {
+            selectItemAndGoBack(item, app: app)
+        }
+        //Test if all cell has beem created.
+        //scrollToLastCell(app)
+    }
+    
+    /**
+     Select an iten, scrolls to show his information.
+     
+     - parameter itemTitle: String with the item name
+     - parameter app:       XCUIApplication that is runing.
+     */
+    func selectItemAndGoBack(itemTitle:String, app: XCUIApplication){
+        app.tables.staticTexts[itemTitle].tap()
+        scrollToLastCell(app)
+        //snapshot(itemTitle+1)\
+        app.navigationBars[itemTitle].buttons["Champions List"].tap()
+    }
+    
+    /**
+     Scroll to the last item of the table.
+     
+     - parameter app: XCUIApplication that is runing
+     */
+    func scrollToLastCell(app: XCUIApplication) {
+        let app = XCUIApplication()
+        let table = app.tables.elementBoundByIndex(0)
+        let lastCell = table.cells.elementBoundByIndex(table.cells.count-1)
+        table.scrollToElement(lastCell)
+    }
+}
+
+extension XCUIElement {
+    func scrollToElement(element: XCUIElement) {
+        while !element.visible() {
+            swipeUp()
+        }
+    }
+    
+    func visible() -> Bool {
+        guard self.exists && !CGRectIsEmpty(self.frame) else { return false }
+        return CGRectContainsRect(XCUIApplication().windows.elementBoundByIndex(0).frame, self.frame)
     }
     
 }
