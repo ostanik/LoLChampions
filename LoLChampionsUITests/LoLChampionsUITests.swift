c9ae79980a5a8cd22f66beeba7f365b3703df846
Ostanik <alan.ostanik@socialbase.com.br>
2016-09-28 17:25:44 -0300

    Testes de UI

--- LoLChampionsUITests/LoLChampionsUITests.swift
+++ LoLChampionsUITests/LoLChampionsUITests.swift
@@ -13,15 +13,9 @@ class LoLChampionsUITests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        //        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
 
     override func tearDown() {
@@ -36,7 +30,7 @@ class LoLChampionsUITests: XCTestCase {
         let app = XCUIApplication()
         let arrItens = ["Ahri", "Alistar"]
 
-//        snapshot("Lista")
+        snapshot("Lista")
         //Go for the four first itens.
         for item in arrItens {
             selectItemAndGoBack(item, app: app)
@@ -54,9 +48,9 @@ class LoLChampionsUITests: XCTestCase {
     func selectItemAndGoBack(itemTitle:String, app: XCUIApplication){
         app.tables.staticTexts[itemTitle].tap()
         NSThread.sleepForTimeInterval(1)
-//        snapshot("i\(itemTitle)")
+        snapshot("i\(itemTitle)")
         scrollToLastCell(app)
-//        snapshot("i\(itemTitle)1")
+        snapshot("i\(itemTitle)1")
         app.navigationBars[itemTitle].buttons["Lista de Campeões"].tap()
     }
     
